a24faeb24765799cad7a53102bcd7049821656d8
Ricardo Quirino <ricardojvquirino@gmail.com>
2015-06-01 16:25:44 +0100

    Correcção

--- projecto4.grupo1.albertoricardo/p4-ejb/src/main/java/projecto4/grupo1/albertoricardo/PlaylistEJBLocal.java
+++ projecto4.grupo1.albertoricardo/p4-ejb/src/main/java/projecto4/grupo1/albertoricardo/PlaylistEJBLocal.java
@@ -2,6 +2,7 @@ package projecto4.grupo1.albertoricardo;
 
 
 import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Local;
 
@@ -16,13 +17,9 @@ public interface PlaylistEJBLocal {
     void remove(PlaylistEntity playlist);
     PlaylistEntity find(Object id);
     void addPlaylist(String name, Date insertDate, UserEntity userlogged);
-<<<<<<< .merge_file_a02788
     boolean findName(String name);
-=======
     List<PlaylistEntity> getPlaylists();
-	void addPlaylist(String name, Date insertDate);
 	List<PlaylistEntity> getOwnPlaylists(int id);
->>>>>>> .merge_file_a07280
     
     
 }
