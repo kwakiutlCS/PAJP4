df8647ab174e76d997bb718a67301e11b4186dc3
Ricardo Quirino <ricardojvquirino@gmail.com>
2015-06-02 18:31:03 +0100

    Remover músicas da playlist

--- projecto4.grupo1.albertoricardo/p4-web/src/main/java/projecto4/grupo1/albertoricardo/playlist/ListPlaylist.java
+++ projecto4.grupo1.albertoricardo/p4-web/src/main/java/projecto4/grupo1/albertoricardo/playlist/ListPlaylist.java
@@ -1,6 +1,7 @@
 package projecto4.grupo1.albertoricardo.playlist;
 
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -9,6 +10,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import projecto4.grupo1.albertoricardo.MusicEntity;
 import projecto4.grupo1.albertoricardo.PlaylistEJB;
@@ -25,6 +29,8 @@ public class ListPlaylist implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	private static Logger log = LoggerFactory.getLogger(ListPlaylist.class);
 	@EJB
 	private PlaylistEJB plistejb;
 
@@ -72,16 +78,26 @@ public class ListPlaylist implements Serializable {
 				int s2 = selectedPlaylist.getMusics().size();
 				System.out.println("DIFERENÇA: "+(s2-s1));
 				plistejb.update(selectedPlaylist);
-				FacesMessage msg = new FacesMessage("Música "+music.getTitle()+" adicionada com successo à playlist seleccionada.");
+				FacesMessage msg = new FacesMessage("Playlists","Música "+music.getTitle()+" adicionada com successo à playlist seleccionada.");
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 			} catch (Exception e) {
-				FacesMessage msg = new FacesMessage("Erro: "+e.getMessage());
+				FacesMessage msg = new FacesMessage("Erro",e.getMessage());
 				FacesContext.getCurrentInstance().addMessage(null, msg);
 			}
 		}
 	}
-	
+
 	public void removeMusicFromPlaylist(PlaylistEntity playlist, MusicEntity music) {
+		Iterator<MusicEntity> it = playlist.getMusics().iterator();
+		while(it.hasNext()) {
+			MusicEntity m = it.next();
+			if (m.getId() == music.getId()) it.remove();
+		}
+
+		plistejb.update(playlist);
+		FacesMessage msg = new FacesMessage("Playlist","Música "+music.getTitle()+" removida com sucesso da playlist "+playlist.getName());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		log.warn("Música "+music.getTitle()+" removida da playlist "+playlist.getName()+" do utilizador "+playlist.getUserOwner().getName());
 	}
 
 	public void setPlaylists(List<PlaylistEntity> playlists) {
